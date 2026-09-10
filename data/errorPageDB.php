69316d4b3e5a9c0e1fa99b6e431d997e627107fb
vrw93 <vrwtutor@gmail.com>
2026-09-10 08:43:49 +0700

    Adding Error Page Detail

--- data/errorPageDB.php
+++ data/errorPageDB.php
@@ -11,5 +11,33 @@ $datas = [
     '404' => [
         'title' => '404',
         'description' => 'Halaman Tidak Ditemukan<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '403' => [
+        'title' => '403',
+        'description' => 'Di Tolak<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '401' => [
+        'title' => '401',
+        'description' => 'Di Tolak<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '429' => [
+        'title' => '429',
+        'description' => 'Terlalu Banyak Request<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '500' => [
+        'title' => '500',
+        'description' => 'Kesalahan Internal<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '502' => [
+        'title' => '502',
+        'description' => 'Bad Gateaway<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '503' => [
+        'title' => '503',
+        'description' => 'Service Tidak Tersedia<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
+    ],
+    '504' => [
+        'title' => '504',
+        'description' => 'Timeout<br>Silahkan cek ulang tautan anda atau kembali ke halaman utama'
     ]
 ];
