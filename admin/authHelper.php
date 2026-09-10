0f975f0aa181c3cecd86f65500a49c2a249c26ce
vrw93 <vrwtutor@gmail.com>
2026-09-10 10:25:22 +0700

    Fixing bug where there was infinited number of redirect

--- admin/authHelper.php
+++ admin/authHelper.php
@@ -3,12 +3,12 @@ $url = $_SERVER['REQUEST_URI'];
 
 if (isset($_SESSION['user_id'])){
     if($_SESSION['role_id'] !== 1){
-        header("Location: /login?url=$url");
+        $type = 'no_admin';
+        include __DIR__ . '/../errorpage.php';
         exit();
     }
 }else if(!isset($_SESSION['user_id'])){
-    $type = 'no_admin';
-    include __DIR__ . '/../errorpage.php';
+    header("Location: /login?url=$url");
     exit();
 }
 ?>
--- admin/users/all.php
+++ admin/users/all.php
@@ -1,15 +1,7 @@
 <?php
 session_start();
 
-if (isset($_SESSION['user_id'])){
-    if($_SESSION['role_id'] !== 1){
-        header("Location: /login?url=/admin");
-        exit();
-    }
-}else if(!isset($_SESSION['user_id'])){
-    header("Location: /login?url=/admin");
-    exit();
-}
+require_once __DIR__ . '/../authHelper.php';
 
 $currentPage = "";
 require_once __DIR__ . "/../../data/adminmenudb.php";
