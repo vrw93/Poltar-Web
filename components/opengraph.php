aff6077c9999783c2457d6d39a43e7473ba05cae
vrw93 <vrwtutor@gmail.com>
2026-09-20 11:40:53 +0700

    Fix og Description

--- components/opengraph.php
+++ components/opengraph.php
@@ -1,19 +1,20 @@
 <?php
 $ogUrl = 'https://poltar.smkn1giritontro.sch.id'
     . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 $ogImage = !empty($currentPost['image'])
     ? 'https://poltar.smkn1giritontro.sch.id' . $currentPost['image']
     : 'https://poltar.smkn1giritontro.sch.id/static/image/poltarLogo.webp';
-
 $ogType = isset($currentPost) ? 'article' : 'website';
+$ogDescription = isset($currentPost['description']) 
+    ? $currentPost['description'] 
+    : "Sebuah platform berbasis web untuk Polisi Taruna SMK Negeri 1 GIRITONTRO yang menyediakan blog, galeri, dan sistem pendaftaran keanggotaan Poltar.";
 ?>
 
 <meta property="og:site_name" content="Poltar Web">
 
 <meta
     property="og:description"
-    content="Sebuah platform berbasis web untuk Polisi Taruna SMK Negeri 1 GIRITONTRO yang menyediakan blog, galeri, dan sistem pendaftaran keanggotaan Poltar."
+    content="<?=htmlspecialchars($ogDescription, ENT_QUOTES, 'UTF-8')?>"
 >
 
 <meta
